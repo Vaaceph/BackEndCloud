2e52c4b93f226cb098d772a6d3d16cb5b6f1c23c
Khan <muhammad.vaaceph@gmail.com>
2018-09-20 01:06:27 +0200

    removed keys

--- app/config/s3.config.js
+++ app/config/s3.config.js
@@ -2,8 +2,6 @@ const AWS = require('aws-sdk');
 const env = require('./s3.env.js');
  
 const s3 = new AWS.S3({
-    accessKeyId: env.AWS_ACCESS_KEY,
-    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
 	region : env.REGION
 });
 
--- app/config/s3.env.js
+++ app/config/s3.env.js
@@ -1,6 +1,4 @@
 const env = {
-	AWS_ACCESS_KEY: 'AKIAJPH5OJ3Y2HBORONQ', // change to yours
-	AWS_SECRET_ACCESS_KEY: 'InAhHNbj/PlsLTIpd90DSG8e72+B0QPSZ5oRLvac', // change to yours
 	REGION : 'us-east-1', // change to yours
 	Bucket: 'saed-files' // change to yours
 };
