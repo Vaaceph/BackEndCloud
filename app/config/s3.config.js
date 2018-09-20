1f7557fcc8d9129126eba54fabf418355311437a
Khan <muhammad.vaaceph@gmail.com>
2018-09-20 18:58:10 +0200

    getting aws keys from environment variables

--- app/config/s3.config.js
+++ app/config/s3.config.js
@@ -2,6 +2,8 @@ const AWS = require('aws-sdk');
 const env = require('./s3.env.js');
  
 const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 	region : env.REGION
 });
 
