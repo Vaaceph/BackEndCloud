3fcff11364bc41798bdb6468e27ae88ecae79ae5
Khan <muhammad.vaaceph@gmail.com>
2018-09-20 19:29:17 +0200

    Logging Envirnoment variables

--- app/config/s3.config.js
+++ app/config/s3.config.js
@@ -1,8 +1,9 @@
 const AWS = require('aws-sdk');
 const env = require('./s3.env.js');
- 
+ console.log("***** process.env.AWS_ACCESS_KEY_ID: ", process.env.AWS_ACCESS_KEY_ID);
+ console.log("***** process.env.AWS_SECRET_ACCESS_KEY: ", process.env.AWS_SECRET_ACCESS_KEY);
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_ID,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 	region : 'us-east-1'
 });
