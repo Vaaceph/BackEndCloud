be6ebeb1d58e244a6080dbafd51f7587d873fbc6
Khan <muhammad.vaaceph@gmail.com>
2018-09-20 02:51:57 +0200

    cors adding 1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const cors = require('cors')
 const corsOptions = {
-  origin: 'http://localhost:4200',
+  origin: ['http://localhost:4200', 'my-balancer-1996715571.us-west-2.elb.amazonaws.com:8080'],
   optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions))
