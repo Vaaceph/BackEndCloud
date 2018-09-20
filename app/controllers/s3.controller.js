fff8f3595aedd196d7a11b5ce6fef8a52b9e0bc5
Khan <muhammad.vaaceph@gmail.com>
2018-09-20 20:39:39 +0200

    remove redundunt S3 config

--- app/controllers/s3.controller.js
+++ app/controllers/s3.controller.js
@@ -12,13 +12,8 @@ const connection = mysql.createConnection({
 
 exports.doUpload = (req, res) => {
 	console.log("***** process.env: ", process.env);
-	console.log("***** process.env.AWS_ACCESS_KEY_ID: ", process.env.AWS_ACCESS_KEY_ID);
+	console.log("***** process.env.AWS_ACCESS_ID: ", process.env.AWS_ACCESS_ID);
  	console.log("***** process.env.AWS_SECRET_ACCESS_KEY: ", process.env.AWS_SECRET_ACCESS_KEY);
-	const s3 = new AWS.S3({
-		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-		region : 'us-east-1'
-	});
 	console.log(s3);
 	const params = {
 		Bucket: s3.bucket,
