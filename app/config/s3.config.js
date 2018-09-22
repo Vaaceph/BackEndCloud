b8094a3b6f10583da70cd70b4ded9c2406d19762
Khan <muhammad.vaaceph@gmail.com>
2018-09-22 23:23:24 +0200

    IAM Role For S3

--- app/config/s3.config.js
+++ app/config/s3.config.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    // accessKeyId: process.env.AWS_ACCESS_ID,
+    // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region : 'us-east-1'
 });
 
